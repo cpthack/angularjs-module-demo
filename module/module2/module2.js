ac4417066357068d8ed09afc093c9dd40a97bbb4
全中国只有一个成佩涛 <1044559878@qq.com>
2017-12-16 00:15:36 +0800

    module2拆分控制器.

--- module/module2/module2.js
+++ module/module2/module2.js
@@ -1,23 +1,29 @@
 /**
  * 测试模块 - module2
  */
-angular.module('module2', []).config(
-		function($stateProvider, $urlRouterProvider) {
 
-			$stateProvider.state('module2', {
-				url : '/module2',
-				templateUrl : 'module/module2/module2.html',
-				controller : 'module2Controller',
-				css : 'module/module2/module2.css'
-			})
+var module2 = angular.module('module2', [ 'oc.lazyLoad' ]);
 
-		})
+module2.config(function($stateProvider, $urlRouterProvider) {
 
-.controller('module2Controller',
-		[ '$scope', "$state", function($scope, $state) {
-			$scope.moduleName = "module2";
-			$scope.test = function() {
-				alert("my name is test of " + $scope.moduleName + ".");
-			};
+	$stateProvider.state('module2', {
+		url : '/module2',
+		templateUrl : 'module/module2/module2.html',
+		controller : 'module2Controller',
+		css : 'module/module2/module2.css',
+		resolve : {
+			loadMyCtrl : [ '$ocLazyLoad', function($ocLazyLoad) {
+				$ocLazyLoad.load([// 添加额外依赖文件
+				// {type: 'css', path: 'testModuleCtrl'},
+				// {type: 'html', path: 'testModuleCtrl.html'},
+				 {type: 'js', path: 'https://cdn.bootcss.com/jquery.form/4.2.2/jquery.form.min.js'}
+				], {
+					cache : true,
+					timeout : 5000
+				});
+				return $ocLazyLoad.load('module/module2/module2Controller.js')
+			} ]
+		}
+	})
 
-		} ]);
+});
---
+++ module/module2/module2Controller.js
@@ -0,0 +1,10 @@
+var module2 = angular.module('module2', []);
+
+module2.controller('module2Controller',
+		[ '$scope', "$state", function($scope, $state) {
+			$scope.moduleName = "module2";
+			$scope.test = function() {
+				alert("my name is test of " + $scope.moduleName + ".");
+			};
+
+		} ]);
