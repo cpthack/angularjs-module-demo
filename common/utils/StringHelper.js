7526964011e4c1c8575cd18547d97a9d4188aae4
全中国只有一个成佩涛 <1044559878@qq.com>
2017-12-16 21:33:09 +0800

    完善字符串辅助工具服务.

--- common/utils/StringHelper.js
+++ common/utils/StringHelper.js
@@ -8,9 +8,37 @@ StringHelper.service('StringHelper', function() {
 	this.isEmpty = function(str) {
 		return str == null || str == '' || typeof str == undefined;
 	}
-	
+
 	this.notEmpty = function(str) {
 		return !this.isEmpty(str);
+	}
+
+	this.isBlank = function(str) {
+		return str == null || /^\s*$/.test(str);
+	}
+
+	this.isNotBlank = function(str) {
+		return !this.isBlank(str);
+	}
+
+	this.trim = function(str) {
+		return str.replace(/^\s+|\s+$/, '');
+	}
+
+	this.startsWith = function(str, prefix) {
+		return str.indexOf(prefix) === 0;
+	}
+
+	this.endsWith = function(str, suffix) {
+		return str.lastIndexOf(suffix) === 0;
+	}
+
+	this.contains = function(str, search) {
+		return this.indexOf(search) >= 0;
+	}
+
+	this.equals = function(str1, str2) {
+		return str1 == str2;
 	}
 
 });
